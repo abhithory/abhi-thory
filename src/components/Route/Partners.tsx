88aa3fa79aafb10e40e598a4b489ea685f46d329
abhithory <abhithory@gmail.com>
2023-11-29 22:03:38 -0600

    reviews section added

--- src/components/Route/Partners.tsx
+++ src/components/Route/Partners.tsx
@@ -6,40 +6,27 @@ import Marquee from "react-fast-marquee";
 
 type Props = {};
 
-const partners = [
-  {
-    url: "https://pixner.net/aikeu/assets/images/partner/one.png",
-  },
-  {
-    url: "https://pixner.net/aikeu/assets/images/partner/two.png",
-  },
-  {
-    url: "https://pixner.net/aikeu/assets/images/partner/three.png",
-  },
-  {
-    url: "https://pixner.net/aikeu/assets/images/partner/four.png",
-  },
-  {
-    url: "https://pixner.net/aikeu/assets/images/partner/five.png",
-  },
-  {
-    url: "https://pixner.net/aikeu/assets/images/partner/one.png",
-  },
-  {
-    url: "https://pixner.net/aikeu/assets/images/partner/two.png",
-  },
-  {
-    url: "https://pixner.net/aikeu/assets/images/partner/three.png",
-  },
-  {
-    url: "https://pixner.net/aikeu/assets/images/partner/four.png",
-  },
-  {
-    url: "https://pixner.net/aikeu/assets/images/partner/five.png",
-  },
-];
-
 const Reviews = (props: Props) => {
+
+  const renderReviews = () => {
+    const reviews = [];
+
+    for (let i = 1; i <= 14; i++) {
+      reviews.push(
+        <Image
+          key={i}
+          src={`/abhi/reviews/${i}.png`}
+          alt=""
+          width={600}
+          height={200}
+          className="mx-4 w-[600px] object-contain  cursor-pointer border-secondary border-4 rounded-2xl  transition-transform hover:scale-105"
+        />
+      );
+    }
+
+    return reviews;
+  };
+
   return (
     <div className="py-10">
       <h1 className={`${styles.heading} font-Monserrat text-center`}>
@@ -48,17 +35,8 @@ const Reviews = (props: Props) => {
       <div className="w-full flex justify-center pt-3">
         <div className="w-[50px] h-[2px] bg-primary" />
       </div>
-      <Marquee className="w-full my-10">
-        {partners.map((i, index) => (
-          <Image
-            src={i.url}
-            alt=""
-            width={100}
-            height={100}
-            key={index}
-            className="mx-14 grayscale-[100%] w-[120px] h-[120px] object-contain hover:grayscale-0 transition-opacity cursor-pointer"
-          />
-        ))}
+      <Marquee className="w-full my-10" speed={120}>
+        {renderReviews()}
       </Marquee>
     </div>
   );
